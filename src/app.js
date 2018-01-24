6efa0b6182fc872c8cd428a619762836bd6a27ae
abhishek <abhishekg@wayforward.io>
2018-01-24 15:46:22 +0530

    adding files

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("app.js is running");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+
+const app = document.getElementById('app');
+ReactDOM.render(<p>Admin app</p>,app);
